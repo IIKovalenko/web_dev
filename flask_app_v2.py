b98327b77ccb8909658a7ab8285ada1985375dc8
petrovao87 <petrovao87@mail.ru>
2019-03-30 13:32:58 +0300

    add base to flask app

--- flask_app_v2.py
+++ flask_app_v2.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = r"C:\projects\web_dev\uploads"
@@ -9,9 +9,11 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -29,31 +31,27 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+            return redirect(url_for('uploaded_file', filename=filename))
 
-@app.route('/test', methods=['GET', 'POST'])
-def redir():
-    return '''
-       <!doctype html>
-       <title>Upload new File</title>
-       <h1>Upload new File</h1>
-       '''
+    return render_template('index.html')
 
 
-@app.route('/<filename>')
+@app.route('/mediafiles/<filename>')
 def uploaded_file(filename):
     print (filename)
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+
+
+
+@app.route('/show/<filename>')
+def show_uploaded_file(filename):
+    filename = 'http://127.0.0.1:5000/uploads/' + filename
+    return render_template('template.html', filename=filename)
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 
 if __name__ == '__main__':
