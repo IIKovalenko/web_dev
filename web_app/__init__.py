f2ad0556b9893653788bcfe0159d4b16d518698a
petrovao87 <petrovao87@mail.ru>
2019-04-04 17:44:42 +0300

    flask app for download files to server + database

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
 from web_app.weather import weather_by_city
-from web_app.python_org_news import get_python_news
+#from web_app.python_org_news import get_python_news
+from web_app.model import db, News
+
+
 
 
 def create_app():
-
-
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-
+    db.init_app(app)
     @app.route('/')
     def index():
         title = 'Прогноз погоды'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         print(weather)
-        news_list = get_python_news()
+        news_list = News.query.order_by(News.published.desc()).all()
         #if weather:
             #weather_text = f'Погода: {weather["temp_C"]}, ощущается как {weather["FeelsLikeC"]}'
         #else:
--- web_app/python_org_news.py
+++ web_app/python_org_news.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from web_app.model import db, News
 
 def get_html(url):
     try:
@@ -23,14 +25,22 @@ def get_python_news():
             title = news.find('a').text
             url = news.find('a')['href']
             published = news.find('time').text
-            result_news.append({
-                'title': title,
-                'url': url,
-                'published': published
-            })
+            print(type(published))
+            try:
+                published = datetime.strptime(published, '%B %d, %Y')
+                print(published)
+            except(ValueError):
+                published = datetime.now()
+            save_news(title, url, published)
 
-        return result_news
-    return False
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news = News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit( )
 
 if __name__ == '__main__':
     #html = get_html('https://www.python.org/blogs/')
