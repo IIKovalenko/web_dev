c34a372f8edd6bf97d8bd567b416f948393cec4f
petrovao87 <petrovao87@mail.ru>
2019-05-17 12:59:39 +0300

    refactoring web_app, add blueprints

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template
 from web_app.weather import weather_by_city
-#from web_app.python_org_news import get_python_news
-from web_app.model import db, News, User
-from web_app.forms import LoginForm
-from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from web_app.model import db, News
+from web_app.user.models import User
+
+from flask_login import LoginManager, current_user, login_required
+from web_app.user.views import blueprint as user_blueprint
 
 
 
@@ -14,7 +15,8 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'user.login'
+    app.register_blueprint(user_blueprint)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -31,34 +33,6 @@ def create_app():
         #else:
             #weather_text = 'Сервер с погодой временно недоступен'
         return render_template("index.html", page_title=title, weather=weather, news_list=news_list)
-
-    @app.route('/login')
-    def login():
-        if current_user.is_authenticated:
-            return redirect(url_for('index'))
-        title = 'Авторизация'
-        login_form = LoginForm()
-        return render_template('login.html', page_title=title, form=login_form)
-
-    @app.route('/process-login', methods=['POST'])
-    def process_login():
-        form = LoginForm()
-
-        if form.validate_on_submit():
-            user = User.query.filter(User.username == form.username.data).first()
-            if user and user.check_password(form.password.data):
-                login_user(user)
-                flash('Вы вошли на сайт')
-                return redirect(url_for('index'))
-
-        flash('Неправильное имя пользователя или пароль')
-        return redirect(url_for('login'))
-
-    @app.route('/logout')
-    def logout():
-        logout_user()
-        flash('Вы успешно разлогинились')
-        return redirect(url_for('index'))
 
     @app.route('/admin')
     @login_required
